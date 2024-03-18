4fe5e840d0edd904cf5c2f2321a149eea8c27675
wyf-cyber <wyf17326280236@gmail.com>
2024-03-18 16:34:05 +0800

    note

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
-def getTime(s):
-    month = int(s[5:7])
-    day = int(s[8:10])
-    h = int(s[11:13])
-    m = int(s[14:16])
-    s = int(s[17:19])
-    return (month,day,h,m,s)
+def get_sum(nums,step):
+    sum_list = []
+    temp = 0
+    # the pointer called i is actually the right pointer,the left pointer's value is i-step+1
+    for i in range(len(nums)):
+        if i < step - 1:
+            temp += nums[i]
+        elif i == step-1:
+            temp += nums[i]
+            sum_list.append(temp)
+        else:
+            temp -= nums[i-step]
+            temp += nums[i]
+            sum_list.append(temp)
+    return sum_list
 
-t = []
-f = open('D:\Algorithm\PythonExercise\source.txt')
-for line in f:
-    t.append(getTime(line))
-t.sort()
-isB = 1
-pre = 0
-ans = 0
-for i in t:
-    if isB:
-        pre = (i[2]*60 + i[3])*60 + i[4]
-        isB = 0
-    else:
-        ans += ((i[2]*60 + i[3])*60 + i[4] - pre)
-        isB = 1
-print(ans)
+nums = [1,2,3,4,5,6,7]
+print(get_sum(nums,3))
---
+++ 基础算法模板汇总/Python常见报错分析.md
@@ -0,0 +1,10 @@
+# Python常见报错分析
+## 段错误
+### 一、expected an indented block
+> 某行未按要求进行缩进
+
+## 逻辑错误
+### 二、RecursionError: maximum recursion depth exceeded
+> 递归深度超出999，可以使用sys标准库中的 setrecursionlimit() 函数修改最大递归深度
+
+### 三、
